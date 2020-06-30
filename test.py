a288ebf18cdb89da7c8d473c64c9095178334d58
Andrew Isaac Williams <ando600@et.byu.edu>
2020-06-30 14:05:24 -0600

    Can move to position successfully.

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 import os
 os.environ['PATH'] = "C:\\Program Files\\ThorLabs\\Kinesis" + ";" + os.environ['PATH']
 #print(os.environ['PATH'])
-
+from threading import Thread
 import thorlabs_kinesis as tk
 import time
 from ctypes import (
     c_short,
     c_char_p,
+    c_void_p,
     byref,
     c_int,
     create_string_buffer,
 )
 from ctypes.wintypes import (
     DWORD,
+    WORD,
 )
+class Static_Vars:
+    steps_per_mm = 34304
 
 from thorlabs_kinesis import kcube_dcservo as kcdc
 
@@ -28,16 +32,45 @@ if kcdc.TLI_BuildDeviceList() == 0:
     serialnos = list(filter(None, serialnos.value.decode("utf-8").split(',')))
     print("Serial #'s:", serialnos)
 
-    # serialno = c_char_p(bytes("27504851", "utf-8"))
+    serialno = c_char_p(bytes("27504851", "utf-8"))
+    accel_param = c_int()
+    vel_param = c_int()
+    message_type = WORD()
+    message_id = WORD()
+    message_data = DWORD()
+    current_motor_pos = 0
 
-    # kcdc.CC_Open(serialno)
-    # kcdc.CC_StartPolling(serialno, c_int(200))
-    # kcdc.CC_ClearMessageQueue(serialno)
-    # # time.sleep(3)
-    # # homeable = bool(kcdc.CC_CanHome(serialno))
-    # # print(homeable)
-    # # print(int(kcdc.CC_GetJogVelParams(serialno)))
-    # # print(int(kcdc.CC_GetPosition(serialno)))
-    # # kcdc.CC_Home(serialno)
-    # kcdc.CC_StopPolling(serialno)
-    # kcdc.CC_Close(serialno)
+    move_pos=1000
+    motor_command = c_int(move_pos)
+
+    # Open Communication
+    kcdc.CC_Open(serialno)
+    kcdc.CC_StartPolling(serialno, c_int(20))
+    kcdc.CC_ClearMessageQueue(serialno)
+    time.sleep(3)
+    homeable = bool(kcdc.CC_CanHome(serialno))
+    print(homeable)
+    #Get Motor Position
+    kcdc.CC_GetJogVelParams(serialno, byref(accel_param), byref(vel_param))
+    #print(accel_param.value)
+    motorPos = kcdc.CC_GetPosition(serialno)
+    print(current_motor_pos)
+    # #kcdc.CC_Home(serialno)
+
+    # Start Move Test
+    kcdc.CC_ClearMessageQueue(serialno)
+    kcdc.CC_MoveToPosition(serialno, motor_command)
+    kcdc.CC_WaitForMessage(serialno, byref(message_type), byref(message_id), byref(message_data))
+
+    while (int(message_type.value) != 2) or (int(message_id.value) != 1):
+        kcdc.CC_WaitForMessage(serialno, byref(message_type), byref(message_id), byref(message_data))
+        kcdc.CC_RequestPosition(serialno)
+        print("TEST", kcdc.CC_GetPosition(serialno))
+
+    kcdc.CC_RequestPosition(serialno)
+    time.sleep(0.1)
+    motorPos = kcdc.CC_GetPosition(serialno)
+    print(current_motor_pos)
+    # Close Communication
+    kcdc.CC_StopPolling(serialno)
+    kcdc.CC_Close(serialno)
